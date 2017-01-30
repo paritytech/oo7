0f7615af6a0d12a7b1d18ac6a5e7b15e204dbb65
Gav <gavin@parity.io>
2017-01-30 21:02:14 +0100

    Much stuff.

--- packages/oo7-react/src/index.jsx
+++ packages/oo7-react/src/index.jsx
@@ -3,10 +3,10 @@ import TextField from 'material-ui/TextField';
 import {Bond, TimeBond, TransformBond} from 'oo7';
 
 export class ReactiveComponent extends React.Component {
-	constructor(reactiveProps = [], extraState = {}) {
+	constructor(reactiveProps = [], bonds = {}) {
 		super();
 		this.reactiveProps = reactiveProps;
-		this.extraState = extraState;
+		this.bonds = bonds;
 	}
 	componentWillMount() { this.initProps(); }
 	componentWillReceiveProps(nextProps) { this.updateProps(nextProps); }
@@ -14,16 +14,16 @@ export class ReactiveComponent extends React.Component {
 	initProps () {
 		this.manageProps({}, this.props);
 		let that = this;
-		Object.keys(this.extraState).forEach(f => {
-			if (this.extraState[f] instanceof Bond)
-				this.extraState[f].subscribe(a => {
+		Object.keys(this.bonds).forEach(f => {
+			if (this.bonds[f] instanceof Bond)
+				this.bonds[f].subscribe(a => {
 					var s = that.state || {};
 					s[f] = a;
 //					console.log(`Setting state via subscription: ${f} => ${a}`);
 					that.setState(s);
 				});
-			else if (this.extraState[f] instanceof Promise)
-				this.extraState[f].then(a => {
+			else if (this.bonds[f] instanceof Promise)
+				this.bonds[f].then(a => {
 					var s = that.state || {};
 					s[f] = a;
 //					console.log(`Setting state via subscription: ${f} => ${a}`);
@@ -32,7 +32,7 @@ export class ReactiveComponent extends React.Component {
 			else {
 				if (s === {})
 					s = that.state || {};
-				s[f] = this.extraState[f];
+				s[f] = this.bonds[f];
 			}
 		})
 	}
@@ -46,20 +46,16 @@ export class ReactiveComponent extends React.Component {
 				if (props[f] instanceof Bond)
 					props[f].drop();
 
+				let update = a => {
+					var s = that.state || {};
+					s[f] = a;
+//					console.log(`Setting state via subscription: ${f} => ${a}`);
+					that.setState(s);
+				};
 				if (nextProps[f] instanceof Bond)
-					nextProps[f].subscribe(a => {
-						var s = that.state || {};
-						s[f] = a;
-//						console.log(`Setting state via subscription: ${f} => ${a}`);
-						that.setState(s);
-					});
+					nextProps[f].subscribe(update);
 				else if (nextProps[f] instanceof Promise)
-					nextProps[f].then(a => {
-						var s = that.state || {};
-						s[f] = a;
-//						console.log(`Setting state via subscription: ${f} => ${a}`);
-						that.setState(s);
-					});
+					nextProps[f].then(update);
 				else {
 					if (s === {})
 						s = this.state || {};
@@ -72,63 +68,73 @@ export class ReactiveComponent extends React.Component {
 	}
 }
 
-export class Reactive extends ReactiveComponent {
-    constructor() { super(['value', 'className']); }
-
+export class ReactiveSpan extends ReactiveComponent {
+    constructor() { super(['className', 'style', 'children']); }
 	render() {
-        let className = typeof(this.state.className) === 'function' ?
-            this.state.className(this.state.value) :
-            typeof(this.state.className) === 'string' ?
-            this.state.className :
-            '';
-        let undefClassName = this.props.undefClassName === null ? '_undefined' : this.props.undefClassName;
-        let undefContent = this.props.undefContent === null ? '?' : this.props.undefContent;
-		if (this.state.value === null || typeof(this.state.value) == 'undefined')
-			return (<span className={undefClassName}>{undefContent}</span>);
-        let a = this.props.transform ? this.props.transform(this.state.value) : this.state.value;
-		return <span className={className}>{a}</span>;
+		return <span
+			className={this.state.className}
+			style={this.state.style}
+			name={this.props.name}
+		>{this.state.children}</span>;
+	}
+}
+
+export class ReactiveDiv extends ReactiveComponent {
+    constructor() { super(['className', 'style', 'children']); }
+	render() {
+		return <div
+			className={this.state.className}
+			style={this.state.style}
+			name={this.props.name}
+		>{this.state.children}</div>;
 	}
 }
 
 export class ReactiveAnchor extends ReactiveComponent {
 	constructor() {
-		super(['href', 'className', 'style', 'children']);
+		super(['href', 'target', 'className', 'style', 'children']);
 	}
 	render() {
 		return <a
 				href={this.state.href}
+				target={this.state.target}
 				className={this.state.className}
 				style={this.state.style}
-				id={this.props.id}
 				name={this.props.name}
-				target={this.props.target}
 			>{this.state.children}</a>;
 	}
 }
 
-export class BondedTextField extends React.Component {
+export class TextBond extends React.Component {
 	constructor() {
 		super();
 		this.state = { value: '' };
 	}
 
+	fixValue(v) {
+		if (this.props.bond instanceof Bond && (typeof(this.props.validator) !== 'function' || this.props.validator(v)))
+			this.props.bond.changed(v);
+		else
+			this.props.bond.changed(undefined);
+	}
+
+	componentWillMount() { this.fixValue(this.state.value); }
+
 	render() {
 		return (<TextField
 			className={this.props.className}
 			style={this.props.style}
-			id={this.props.id}
 			name={this.props.name}
 			value={this.state.value}
 			floatingLabelText={this.props.floatingLabelText}
 			errorText={this.props.errorText || (typeof(this.props.validator) === 'function' && !this.props.validator(this.state.value) ? this.props.invalidText : null)}
 			onChange={(e, v) => {
 				this.setState({value: v});
-				if (this.props.bond instanceof Bond && (typeof(this.props.validator) !== 'function' || this.props.validator(v)))
-					this.props.bond.changed(v);
+				this.fixValue(v);
 			}}
 		/>);
 	}
 }
-BondedTextField.defaultProps = {
+TextBond.defaultProps = {
 	invalidText: 'Invalid'
 };
