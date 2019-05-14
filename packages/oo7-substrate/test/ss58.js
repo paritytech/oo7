3be21f022d718092aeaf02875d61e8179ce1a072
Stefanie Doll <stefie.doll@googlemail.com>
2019-05-14 14:59:24 +0200

    Use Bobs sr25519 key in failing test

--- packages/oo7-substrate/test/ss58.js
+++ packages/oo7-substrate/test/ss58.js
@@ -5,7 +5,7 @@ const { ss58Encode, ss58Decode } = require('../src/ss58');
 describe('ss58', () => {
 	it('should encode & decode back', () => {
 		const encoded = ss58Encode(1);
-		encoded.should.equal('F7L6');
+		encoded.should.equal('F7NZ');
 		const decoded = ss58Decode(encoded);
 		decoded.should.equal(1);
 	});
